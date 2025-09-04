31c39333926b1b442c2c1c7f1edc45c177bd1707
Oriol <oriol.castro@genesy.ai>
2025-09-04 13:00:40 +0200

    chore: cleanup boilerplate and install tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import genesyLogo from './assets/genesy-ai-logo.svg'
-import { Counter } from './components/Counter'
-import { LeadsList } from './components/LeadsList'
-
 function App() {
   return (
-    <main className="main">
+    <main className="max-w-5xl mx-auto p-8 text-center flex flex-col items-center justify-center min-h-[80vh]">
       <div>
         <a href="https://genesy.ai" target="_blank">
-          <img src={genesyLogo} className="logo genesy" alt="Genesy AI logo" />
+          <img 
+            src="https://cdn.prod.website-files.com/674ec82ac8f13332ff5201aa/677eaa5351b3ef0c2418820a_favicon%20256x256.png" 
+            className="h-20 p-4 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" 
+            alt="Genesy AI logo" 
+          />
         </a>
       </div>
-      <h1 className="title">Genesy AI</h1>
-      <Counter />
-
-      <LeadsList />
+      <h1 className="mt-4 text-4xl font-semibold text-gray-900 dark:text-white">TinyGenesy</h1>
     </main>
   )
 }
